0aa70f7a53c2481f0d0df588edd17db51735f0d3
FlaminEon <38511899+FlaminEon@users.noreply.github.com>
2018-05-07 21:12:04 -0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Stat health;
+
+    private void Awake()
+    {
+        health.Initialize();
+    }
+
+    void Start ()
+    {
+	}
+	
+
+	void Update ()
+    {
+        // Aca es donde se agregan las interacciones con triggers o colliders
+	}
+}
---
+++ Stat.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Stat
+{
+    [SerializeField]
+    private BarScript barra;
+
+    [SerializeField]
+    private float maxVal;
+
+    [SerializeField]
+    private float currentVal;
+
+    public float CurrentVal
+    {
+        get
+        {
+            return currentVal;
+        }
+
+        set
+        {
+            this.currentVal = value;
+            barra.Value = currentVal;
+        }
+    }
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+
+        set
+        {
+            this.maxVal = value;
+            barra.MaxValue = maxVal;
+        }
+    }
+
+    public void Initialize()
+    {
+        this.MaxVal = maxVal;
+        this.CurrentVal = currentVal;
+    }
+    
+}
